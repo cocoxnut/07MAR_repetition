f4c0a55128eeb1ead6bad413b18a21af3b8f13a3
cocoxnut <cocoxnut@gmail.com>
2022-03-15 16:50:26 +0600

    add/exercises

--- class16.py
+++ class16.py
@@ -2,7 +2,7 @@ app = input("Applications you know: ")
 age = int(input("Enter your age: "))
 expert = int(input("Enter experience in years: "))
 salary = int(input("Enter your preferred salary: "))
-if app == 'java' and 'python' or 'javascript' and age >= 18 and age <= 65 and expert > 3 and salary <= 60000:
+if app == 'java' or 'python' or 'javascript' and age >= 18 and age <= 65 and expert > 3 and salary <= 60000:
     print("Accepted")
 else:
     print("Not acceptable") 
